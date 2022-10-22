a714a6701c91940da1205b72dabb4db1fe99877b
Emilio Mazola <mazolatec@gmail.com>
2022-10-22 09:10:11 -0300

    Add FuncionarioControle StrinBuilder VendasExercicioProposto

---
+++ StringBuilder/Program.cs
@@ -0,0 +1,34 @@
+﻿using Stringbuilder.Entities;
+using System;
+
+namespace Stringbuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Comment c1 = new Comment("Have a nice trip - Fazer boa viagem!");
+            Comment c2 = new Comment("Wow that's awesome - Uau que incrível!");
+            Post p1 = new Post(
+                    DateTime.Parse("21/06/2018 13:05:44"),
+                    "Traveling to New Zealand - Viajar para a Nova Zelândia",
+                    "I'm going to visit this wonderful country! - Eu vou visitar este país maravilhoso",
+                    12);
+            p1.AddComment(c1);
+            p1.AddComment(c2);
+
+            Comment c3 = new Comment("Good night - Boa noite");
+            Comment c4 = new Comment("May the Force be with you - Que a força esteja com você");
+            Post p2 = new Post(
+                    DateTime.Parse("28/07/2018 23:14:19"),
+                    "Good night guys - Boa noite, galera",
+                    "See you tomorrow - Vejo você amanhã",
+                    5);
+            p2.AddComment(c3);
+            p2.AddComment(c4);
+
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+        }
+    }
+}
